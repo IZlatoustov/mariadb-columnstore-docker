93cd45670921acc22e1a397b58eb1aac54cb2bd5
Ivan Zlatoustov <ivan.zlatoustov@mariadb.com>
2018-07-31 19:26:56 +0300

    Preload the files in the docker image

--- columnstore/bookstore/init.sh
+++ columnstore/bookstore/init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo "Getting the bookstore data ..."
-mkdir -p /tmp/bookstore/csv
-curl https://dl.dropboxusercontent.com/s/0z1iwdkw42cvr3d/fmc2.tar.gz?dl=1 --output /tmp/bookstore/csv/bookstore.tar.gz
-echo "Extracting bookstore files ..."
-tar -vxzf /tmp/bookstore/csv/bookstore.tar.gz --directory /tmp/bookstore/csv/
+# echo "Getting the bookstore data ..."
+# mkdir -p /tmp/bookstore/csv
+# curl https://dl.dropboxusercontent.com/s/0z1iwdkw42cvr3d/fmc2.tar.gz?dl=1 --output /tmp/bookstore/csv/bookstore.tar.gz
+# echo "Extracting bookstore files ..."
+# tar -vxzf /tmp/bookstore/csv/bookstore.tar.gz --directory /tmp/bookstore/csv/
 
 echo "Waiting for columnstore to respond (2m)."
 i=0
@@ -17,4 +17,4 @@ until (/usr/local/mariadb/columnstore/bin/mcsadmin getSystemStatus|grep -c "Syst
         echo "Retry" $i
     done
 echo "Loading Bookstore Sandbox Data ...."
-/usr/local/mariadb/columnstore/mysql/bin/mysql < /tmp/bookstore/sql/load_ax_template.sql
+/usr/local/mariadb/columnstore/mysql/bin/mysql < /tmp/bookstore/csv/load_ax_template.sql
---
+++ columnstore/labs/labs_server.md
@@ -0,0 +1,9 @@
+# Labs
+
+    #DBaaS
+
+## Your database is prepared and the sandbox data is loaded. Lets start by choosing the database we want to work on. In this case **bookstore** database.
+
+```sql
+USE DATABASE bookstore
+```
